d9cccd15dfc30b43c01495460897924f2cca2718
GregCahillC00216328 <gregcahill17@gmail.com>
2019-09-25 11:47:46 +0100

    added project

---
+++ Assets/Scripts/RunningTotalFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningTotalFilter:MonoBehaviour
+{
+    public float[] m_arr;
+    public float[] result=new float[5];
+    
+
+    public void runningCalc(float[] t_arr)
+    {
+        m_arr = t_arr;
+        result = new float[m_arr.Length];
+        for (int i = 0; i < m_arr.Length; i++)
+        {
+            float numToAddTo = m_arr[i];
+            if (i == 0)
+            {
+                result[i] = m_arr[i];
+            }
+            else
+            {
+                result[i] = m_arr[i] + result[i - 1];
+            }
+
+
+            print(result[i]);
+            //runningCalc();
+        }
+    }
+
+    
+
+
+    
+}
---
+++ Assets/Tests/RunningTotalTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    
+    public class RunningTotalTest
+    {
+        // A Test behaves as an ordinary method
+        RunningTotalFilter test=new RunningTotalFilter();
+        [Test]
+        
+        public void RunningTotalTestSimplePasses()
+        {
+            float[] expected = { 1, 4, 9, 16, 25 };
+            test.runningCalc(new float[5] { 1, 3, 5, 7, 9 });
+            
+            CollectionAssert.AreEqual(expected, test.result);
+            // Use the Assert class to test conditions
+        }
+
+        
+    }
+}
